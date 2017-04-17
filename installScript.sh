ec995f5de76e9bbeb41f119e9bf6328342fab450
jack-8 <yasutaka.kouno@gmail.com>
2017-04-17 11:16:53 +0900

    first commit

---
+++ installScript.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+echo "Installing brew..."
+which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+masInstallList=(
+  497799835
+  443987910
+)
+
+brewInstallList=(
+  git
+  mas
+  openssl
+)
+
+brewCaskInstallList=(
+  java
+  google-japanese-ime
+  caffeine
+  appcleaner
+  microsoft-office
+  docker
+  iterm2
+  android-studio
+  google-chrome
+  firefox
+  opera
+  atom
+  inkscape
+  gimp
+)
+
+function masInstall() {
+  if mas install $1 > /dev/null; then
+    echo "installed $1 "
+  else
+    echo "install $1 ..."
+    mas install $1
+  fi
+}
+
+function brewInstall() {
+  if brew ls --versions $1 2> /dev/null; then
+    echo "installed $1"
+  else
+    echo "install $1"
+    brew install $1
+  fi
+}
+
+function brewCaskInstall() {
+  if brew cask ls --versions $1 2> /dev/null; then
+    echo "installed $1"
+  else
+    echo "install $1"
+    brew cask install $1
+  fi
+}
+
+echo "brew doctor..."
+if brew doctor > /dev/null; then
+  echo "ok!"
+
+  for masInstallItem in "${masInstallList[@]}"; do
+    masInstall $masInstallItem
+  done
+
+  for brewInstallItem in "${brewInstallList[@]}"; do
+    brewInstall $brewInstallItem
+  done
+
+  for brewCaskInstallItem in "${brewCaskInstallList[@]}"; do
+    brewCaskInstall $brewCaskInstallItem
+  done
+
+fi
+
+echo "update mas..."
+mas upgrade
+
+echo "update brew..."
+brew upgrade
+
+echo "installed mas list"
+mas list
+
+echo "installed brew list..."
+brew ls
+
+echo "installed brew cask list..."
+brew cask ls
+
+echo "brew cleanup..."
+brew cleanup -s
+
+exit
