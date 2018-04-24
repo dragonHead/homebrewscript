384be98bdc4136e9b6d90a117c1031adb40af0a8
jack-8 <yasutaka.kouno@gmail.com>
2018-04-24 23:24:46 +0900

    update

--- brewInstall.sh
+++ brewInstall.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "Installing brew..."
-which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+HOME_DIR=$(cd $(dirname $0) && pwd)
+FILE_NAME=$(basename $0 .sh)
+
+LOG_DIR=${HOME_DIR}/log
+LOG_FILE=${LOG_DIR}/${FILE_NAME}`date +%Y%m%d%H%M%S`.log
+
+mkdir -p ${LOG_DIR}
 
 brewInstallList=(
   git
@@ -56,43 +61,68 @@ function brewCaskInstall() {
   fi
 }
 
-#echo "brew doctor..."
-#if brew doctor > /dev/null; then
+echo "Installing brew..."
+#which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "brew doctor..."
+if brew doctor > /dev/null; then
   echo "ok!"
 
-  for brewInstallItem in "${brewInstallList[@]}"; do
-    brewInstall $brewInstallItem
-  done
+#  for brewInstallItem in "${brewInstallList[@]}"; do
+#    brewInstall $brewInstallItem
+#  done
 
-  for brewCaskInstallItem in "${brewCaskInstallList[@]}"; do
-    brewCaskInstall $brewCaskInstallItem
-  done
+#  for brewCaskInstallItem in "${brewCaskInstallList[@]}"; do
+#    brewCaskInstall $brewCaskInstallItem
+#  done
 
-  for masInstallItem in "${masInstallList[@]}"; do
-    masInstall $masInstallItem
-  done
+#  for masInstallItem in "${masInstallList[@]}"; do
+#    masInstall $masInstallItem
+#  done
 
-#fi
+fi
 
+echo
 echo "update mas..."
 mas upgrade
+
+echo "###############################################"
+echo
 
 echo "update brew..."
 brew upgrade
 
+echo "###############################################"
+echo
+
 echo "installed mas list"
 mas list
+
+echo "###############################################"
+echo
 
 echo "installed brew list..."
 brew ls
 
+echo "###############################################"
+echo
+
 echo "installed brew cask list..."
 brew cask ls
+
+echo "###############################################"
+echo
 
 echo "brew cleanup..."
 brew cleanup -s
 
+echo "###############################################"
+echo
+
 echo "brew cask cleanup"
 brew cask cleanup
+
+echo "###############################################"
+echo
 
 exit
