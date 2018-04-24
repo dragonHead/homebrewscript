708fcd954373131161f86ec1f8f4c7f3bcb784ad
jack-8 <yasutaka.kouno@gmail.com>
2018-04-24 23:31:51 +0900

    udpate

--- brewInstall.sh
+++ brewInstall.sh
@@ -4,9 +4,7 @@ HOME_DIR=$(cd $(dirname $0) && pwd)
 FILE_NAME=$(basename $0 .sh)
 
 LOG_DIR=${HOME_DIR}/log
-LOG_FILE=${LOG_DIR}/${FILE_NAME}`date +%Y%m%d%H%M%S`.log
-
-mkdir -p ${LOG_DIR}
+LOG_FILE=${LOG_DIR}/${FILE_NAME}_`date +%Y%m%d%H%M%S`.log
 
 brewInstallList=(
   git
@@ -47,7 +45,7 @@ function brewInstall() {
   if brew ls --versions $1 2> /dev/null; then
     echo "installed $1"
   else
-    echo "install $1"
+    echo "install $1 ..."
     brew install $1
   fi
 }
@@ -56,18 +54,19 @@ function brewCaskInstall() {
   if brew cask ls --versions $1 2> /dev/null; then
     echo "installed $1"
   else
-    echo "install $1"
+    echo "install $1 ..."
     brew cask install $1
   fi
 }
+
+echo "Create log directory"
+mkdir -p ${LOG_DIR}
 
 echo "Installing brew..."
 which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "brew doctor..."
 if brew doctor > /dev/null; then
-  echo "ok!"
-
   for brewInstallItem in "${brewInstallList[@]}"; do
     brewInstall $brewInstallItem
   done
@@ -79,50 +78,27 @@ if brew doctor > /dev/null; then
   for masInstallItem in "${masInstallList[@]}"; do
     masInstall $masInstallItem
   done
-
 fi
 
-echo
 echo "update mas..."
 mas upgrade
-
-echo "###############################################"
-echo
 
 echo "update brew..."
 brew upgrade
 
-echo "###############################################"
-echo
-
 echo "installed mas list"
 mas list
-
-echo "###############################################"
-echo
 
 echo "installed brew list..."
 brew ls
 
-echo "###############################################"
-echo
-
 echo "installed brew cask list..."
 brew cask ls
-
-echo "###############################################"
-echo
 
 echo "brew cleanup..."
 brew cleanup -s
 
-echo "###############################################"
-echo
-
-echo "brew cask cleanup"
+echo "brew cask cleanup..."
 brew cask cleanup
-
-echo "###############################################"
-echo
 
 exit
