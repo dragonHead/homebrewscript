517ad7fe595a882ed0d29cfe31b31e91c3f245c7
jack-8 <yasutaka.kouno@gmail.com>
2018-08-25 09:47:30 +0900

    add Brewfile

---
+++ Brewfile
@@ -0,0 +1,37 @@
+tap "heroku/brew"
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/cask-versions"
+tap "homebrew/core"
+cask "java"
+brew "git"
+brew "mas"
+brew "maven"
+brew "node"
+brew "openssl"
+brew "docker"
+brew "socat"
+cask "android-studio"
+caks "appcleaner"
+cask "atom"
+cask "caffeine"
+cask "eclipse-jee"
+cask "docker"
+cask "firefox"
+cask "gimp"
+cask "google-chrome"
+cask "google-japanese-ime"
+cask "opera"
+cask "slack"
+cask "thunderbird"
+cask "vagrant"
+cask "virtualbox"
+cask "xquartz"
+mas "1Password", id: 443987910
+mas "App Icon Resizer", id: 1013592989
+mas "JPEGmini Lite", id: 525742250
+mas "Kindle", id: 405399194
+mas "LINE", id: 539883307
+mas "Magnet", id: 441258766
+mas "The Unarchiver", id: 425424353
+mas "Xcode", id: 497799835
--- brewInstall.sh
+++ brewInstall.sh
@@ -1,76 +1,13 @@
 #!/bin/bash
 
+#https://github.com/Homebrew
+
 HOME_DIR=$(cd $(dirname $0) && pwd)
 FILE_NAME=$(basename $0 .sh)
 
 LOG_DIR=${HOME_DIR}/log
 LOG_FILE=${LOG_DIR}/${FILE_NAME}_`date +%Y%m%d%H%M%S`.log
 
-brewInstallList=(
-  git
-  mas
-  openssl
-  socat
-  docker
-  node
-)
-
-brewCaskInstallList=(
-  android-studio
-  appcleaner
-  atom
-  caffeine
-  eclipse-jee
-  docker
-  firefox
-  gimp
-  google-chrome
-  google-japanese-ime
-  opera
-  slack
-  xquartz
-  virtualbox
-  vagrant
-  microsoft-office
-  thunderbird
-)
-
-masInstallList=(
-  1013592989
-  443987910
-  497799835
-  441258766
-  539883307
-  425424353
-  405399194
-)
-
-function masInstall() {
-  if mas install $1 > /dev/null; then
-    echo "installed $1 "
-  else
-    echo "install $1 ..."
-    mas install $1
-  fi
-}
-
-function brewInstall() {
-  if brew ls --versions $1 2> /dev/null; then
-    echo "installed $1"
-  else
-    echo "install $1 ..."
-    brew install $1
-  fi
-}
-
-function brewCaskInstall() {
-  if brew cask ls --versions $1 2> /dev/null; then
-    echo "installed $1"
-  else
-    echo "install $1 ..."
-    brew cask install $1
-  fi
-}
 
 echo "Create log directory"
 mkdir -p ${LOG_DIR}
@@ -80,17 +17,7 @@ which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubu
 
 echo "brew doctor..."
 if brew doctor > /dev/null; then
-  for brewInstallItem in "${brewInstallList[@]}"; do
-    brewInstall $brewInstallItem
-  done
-
-  for brewCaskInstallItem in "${brewCaskInstallList[@]}"; do
-    brewCaskInstall $brewCaskInstallItem
-  done
-
-  for masInstallItem in "${masInstallList[@]}"; do
-    masInstall $masInstallItem
-  done
+  brew bundle ./Brewfile
 fi
 
 echo "update mas..."
@@ -113,5 +40,8 @@ brew cleanup -s
 
 echo "brew cask cleanup..."
 brew cask cleanup
+
+echo "create Brewfile"
+brew bundle dump
 
 exit
