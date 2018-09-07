167d6cbafaac0ba4a80b05cb98869c09527973ec
jack-8 <yasutaka.kouno@gmail.com>
2018-09-08 01:28:41 +0900

    update

--- brewInstall.sh
+++ brewInstall.sh
@@ -11,7 +11,7 @@ FILE_NAME=$(basename $0 .sh)
 
 LOG_DIR=${HOME_DIR}/log
 LOG_FILE=${LOG_DIR}/${FILE_NAME}_`date +%Y%m%d%H%M%S`.log
-
+exec > >(tee ${LOG_FILE}) 2>&1
 
 echo "Create log directory"
 mkdir -p ${LOG_DIR}
