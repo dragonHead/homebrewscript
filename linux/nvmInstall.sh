346521d607b51cf20b6b574e8df0d792dc906b2d
jack-8 <himawari888ty@gmail.com>
2020-03-21 01:00:58 +0900

    update nvmInstall.sh

--- linux/nvmInstall.sh
+++ linux/nvmInstall.sh
@@ -10,13 +10,6 @@ exec > >(tee ${LOG_FILE}) 2>&1
 echo "Create log directory..."
 mkdir -p ${LOG_DIR}
 
-export NVM_DIR="$HOME/.nvm" && (
-  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-  cd "$NVM_DIR"
-  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-) && \. "$NVM_DIR/nvm.sh"
-
-echo export NVM_DIR=\"'${HOME}'/.nvm\" >> $HOME/.bashrc
-echo "[ -s \${NVM_DIR}/nvm.sh ] && \. \${NVM_DIR}/nvm.sh" >> $HOME/.bashrc
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 exit;
